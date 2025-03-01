7ab09212a4d37bf694313656987404d0350f3897
tuanzen8 <tuan.nguyen@zen8labs.com>
2025-03-02 00:03:51 +0700

    fix chi-tiet-tin-tuc

---
+++ script/chi-tiet-tin-tuc.js
@@ -0,0 +1,9 @@
+import { renderBreadcrumb } from "/components/breadcrumb/breadcrumb.js";
+
+const breadcrumbLinks = [
+  { name: "Tin tức & Sự kiện", url: "index.html" },
+  { name: "Tin tức ", url: "library.html" },
+  { name: "Khoa Thương mại điện tử và Kinh tế số", url: null }, // Active page (không có link)
+];
+
+renderBreadcrumb(breadcrumbLinks);
